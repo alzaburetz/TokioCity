91d097af35bda4d1ba540d833e595d384c663bf4
alzaburetz <anton.motin.dev@gmail.com>
2019-10-18 17:42:51 +0300

    Images are loading

--- TokioCity/TokioCity.Android/MainActivity.cs
+++ TokioCity/TokioCity.Android/MainActivity.cs
@@ -18,10 +18,11 @@ namespace TokioCity.Droid
             ToolbarResource = Resource.Layout.Toolbar;
 
             base.OnCreate(savedInstanceState);
-
+            FFImageLoading.Forms.Platform.CachedImageRenderer.Init(true);
             global::Xamarin.Forms.Forms.SetFlags("CollectionView_Experimental");
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            
             LoadApplication(new App());
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
